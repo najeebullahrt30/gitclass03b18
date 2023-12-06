25aa9b3d92b7b7a8aa76f7bc782407f17b1bffe2
najeebullahrt30 <najeebullahrt30@gmail.com>
2023-12-05 20:08:33 -0500

    second commit

--- src/T1.java
+++ src/T1.java
@@ -2,6 +2,12 @@ public class T1 {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        
 
     }
 }
